23da99979eeda6e8b35fb54470e1472fe4615c57
blue-fatty <690361453@qq.com>
2020-07-27 15:36:59 +0800

    路由的封装

---
+++ src/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        home
+        <nav-header></nav-header>   <!--下面是驼峰，这边是转为-形式-->
+        <router-view></router-view>  <!--这里放子路由-->
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+    import NavHeader from '../components/NavHeader.vue'
+    import NavFooter from '../components/NavFooter.vue'
+    export default {
+        name:'nav-home',
+        components:{
+            NavHeader,
+            NavFooter
+        }
+    }
+</script>
